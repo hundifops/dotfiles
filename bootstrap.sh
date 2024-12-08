14fa51bf3f3143aaa24d4d843de3ebdbb53fe38a
Hundi <cwiley.cs@gmail.com>
2024-12-08 16:27:11 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,17 @@ DOTFILES_DIR="$HOME/dotfiles"
 ln -sf "DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
 ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
+ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
 
 if ! command -v brew &> /dev/null
 then
     echo "Homebrew was not found. Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+if ! command -v brew &> /dev/null
+then
+    echo "Oh-My-Zsh was not found. Installing package..."
+    /bin/bash sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "dotfiles installation complete"
